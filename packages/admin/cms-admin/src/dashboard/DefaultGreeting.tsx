42a79a75e949bef17206bf7245142bf60278b7a9
Ricky Smith <jamesricky@me.com>
2023-06-30 10:27:05 +0200

    Move dashboard header and widgets into cms-admin

---
+++ packages/admin/cms-admin/src/dashboard/DefaultGreeting.tsx
@@ -0,0 +1,25 @@
+import { gql, useQuery } from "@apollo/client";
+import React from "react";
+import { FormattedMessage } from "react-intl";
+
+import { GQLDashboardCurrentUserNameQuery } from "./DefaultGreeting.generated";
+
+const CURRENT_USER_NAME_QUERY = gql`
+    query DashboardCurrentUserName {
+        currentUser {
+            name
+        }
+    }
+`;
+
+export const DefaultGreeting = () => {
+    const { data } = useQuery<GQLDashboardCurrentUserNameQuery>(CURRENT_USER_NAME_QUERY);
+
+    if (data) {
+        return (
+            <FormattedMessage id="comet.dashboard.userGreeting" defaultMessage="Hello {givenName}!" values={{ givenName: data.currentUser.name }} />
+        );
+    }
+
+    return <FormattedMessage id="comet.dashboard.greeting" defaultMessage="Hello!" />;
+};
